e930a1462372cfad1e3c2487530049ae0806e1e7
Mark Heijnekamp <mark@heijnekamp.nl>
2023-02-12 11:35:01 +0100

    Initial Commit

---
+++ RSSClient/RSSClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSClientApp.swift
+//  RSSClient
+//
+//  Created by Mark Heijnekamp on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RSSClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
